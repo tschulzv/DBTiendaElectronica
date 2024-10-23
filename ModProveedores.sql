5551798cc8601291cbb575c772318ad29924a019
tschulzv <139179840+tschulzv@users.noreply.github.com>
2024-10-23 14:00:12 -0300

    modificar tabla proveedores

---
+++ ModProveedores.sql
@@ -0,0 +1,33 @@
+/* SCRIPT MODIFICACIÓN DE TABLA PROVEEDORES 
+Destinado a hacer cumplir la política de la empresa 
+*/
+
+USE TiendaElectronica;
+
+ -- MODIFICACIONES PARA AGREGAR LA COLUMNA 'SALDO' A PROVEEDORES
+ALTER TABLE Proveedores
+ADD saldo numeric(12);
+
+UPDATE Proveedores
+SET saldo = 0
+WHERE saldo IS NULL;
+
+ALTER TABLE Proveedores
+ALTER COLUMN saldo numeric(12) NOT NULL;
+
+-- CONSTRAINT PARA VERIFICAR QUE EL SALDO NO SUPERE A LA LINEA DE CRÉDITO
+ALTER TABLE Proveedores
+ADD CONSTRAINT chk_saldo_vs_credito CHECK (saldo <= linea_credito);
+
+-- Agregar los saldos existentes
+UPDATE Proveedores
+SET saldo = 2500000
+WHERE id_proveedor = 1;
+
+UPDATE Proveedores
+SET saldo = 900000
+WHERE id_proveedor = 5;
+
+UPDATE Proveedores
+SET saldo = 10000000
+WHERE id_proveedor = 2;
