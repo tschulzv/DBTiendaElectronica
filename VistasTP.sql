b9166a8474cb9da93817505b78c58079c876ebde
tschulzv <139179840+tschulzv@users.noreply.github.com>
2024-11-19 11:11:15 -0300

    Update VistasTP.sql

--- VistasTP.sql
+++ VistasTP.sql
@@ -1,15 +1,15 @@
 USE TiendaElectronica;
 GO; -- iniciar un nuevo lote
 /*
-El diseño deberá contener algunas vistas como:
+El diseÃ±o deberÃ¡ contener algunas vistas como:
 - Facturas de proveedores vencidas (1)
 - Stocks de productos. (1)*/
  
 -- Algunas declaraciones, como CREATE VIEW, CREATE PROCEDURE, CREATE FUNCTION, etc., 
--- deben ser las únicas en el lote porque el servidor las trata como operaciones independientes
+-- deben ser las Ãºnicas en el lote porque el servidor las trata como operaciones independientes
 
 -- FACTURAS DE PROVEEDORES VENCIDAS
--- Obs. Sólo se seleccionan las VENCIDAS Y PENDIENTES DE PAGO
+-- Obs. SÃ³lo se seleccionan las VENCIDAS Y PENDIENTES DE PAGO
 CREATE VIEW view_facturas_vencidas AS
 	SELECT c.id_compra AS "Factura", c.fecha_compra AS "Fecha Compra", c.fecha_vencimiento AS "Fecha Vencimiento", pr.nombre AS "Proveedor",  
 		c.total_compra AS "Total", c.saldo_compra AS "Saldo"
@@ -20,8 +20,8 @@ CREATE VIEW view_facturas_vencidas AS
 GO;
 
 -- STOCK DE PRODUCTOS
--- 1. Ver stock de los depósitos por separado
-CREATE VIEW stock_productos AS
+-- 1. Ver stock de los depÃ³sitos por separado
+CREATE VIEW view_stock_productos AS
 	SELECT st.id_stock AS "Stock", de.nombre AS "Deposito", pr.descripcion AS "Producto", 
 	st.cantidad AS "cantidad", pr.ultimo_costo_unitario AS "Ult. Costo Unitario", st.ultima_actualizacion AS "Ult. Actualizacion"
 	FROM Stock st
@@ -30,8 +30,8 @@ CREATE VIEW stock_productos AS
 
 GO;
 
--- 2. Ver el total del stock de ambos depósitos 
-CREATE VIEW stock_total_productos AS
+-- 2. Ver el total del stock de ambos depÃ³sitos 
+CREATE VIEW view_stock_total_productos AS
 	SELECT pr.descripcion AS "Producto", SUM(st.cantidad) AS "Total Cantidad", pr.ultimo_costo_unitario AS "Ult. Costo Unitario", MAX(st.ultima_actualizacion) AS "Ult. Actualizacion"
 	FROM Stock st
 	INNER JOIN Productos pr ON st.id_producto = pr.id_producto
