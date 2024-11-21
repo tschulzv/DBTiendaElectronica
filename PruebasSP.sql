8210a1f559a726e941c2628a5d33a9b3e09f45cc
Eliana Sánchez <152930141+elianasmz@users.noreply.github.com>
2024-11-21 17:57:13 -0300

    Update PruebasSP.sql

--- PruebasSP.sql
+++ PruebasSP.sql
@@ -4,7 +4,7 @@ USE DBTienda;
 -- PROVEEDOR ------------------------------------------------------------
 -- Agregar un Proveedor
 EXEC sp_ManageProveedor
-	@accion = 'A', -- 'A' para alta, 'M' para modificación, 'B' para baja
+	@accion = 'A', -- 'A' para alta, 'M' para modificaciÃ³n, 'B' para baja
     @nombre = 'Compusoft',
 	@id_proveedor = 11,
     @direccion = 'Juan Leon Mallorquin',
@@ -15,16 +15,16 @@ EXEC sp_ManageProveedor
 
 SELECT * FROM Proveedores;
 
--- MODIFICAR EL PROVEEDOR RECIÉN CREADO
+-- MODIFICAR EL PROVEEDOR RECIÃ‰N CREADO
 EXEC sp_ManageProveedor
-	@accion = 'M', --  'M' para modificación
+	@accion = 'M', --  'M' para modificaciÃ³n
     @nombre = 'Compusoft S.R.L',
 	@id_proveedor = 11,
 	@linea_credito = 30000000
 
 SELECT * FROM Proveedores;
 
--- BORRAR EL PROVEEDOR RECIÉN CREADO
+-- BORRAR EL PROVEEDOR RECIÃ‰N CREADO
 EXEC sp_ManageProveedor
 	@accion = 'B', --  'B' para dar de baja
 	@id_proveedor = 11
@@ -32,3 +32,45 @@ EXEC sp_ManageProveedor
 SELECT * FROM Proveedores;
 
 -- PAGO -----------------------------------------------------------
+--Agregar nuevo pago
+EXEC sp_ManagePagos
+    @accion = 'A', --Alta
+    @id_pago = 10,
+    @id_proveedor = 1,
+    @fecha = '2024-04-01',
+    @importe_total = 1000000;
+
+--Modificar el pago
+EXEC sp_ManagePagos
+    @accion = 'M', --modificar
+    @id_pago = 10,
+    @fecha = '2024-04-02', 
+    @importe_total = 1200000; 
+
+--Dar de baja un pago
+EXEC sp_ManagePagos
+    @accion = 'B',--baja
+    @id_pago = 10;
+
+SELECT * FROM Pagos
+	
+-- DATALLES PAGOS -----------------------------------------------------------
+--Dar de alta un detallePago
+EXEC sp_ManageDetallesPago
+    @accion = 'A',--alta
+    @id_detalle_pago = 10,
+    @id_pago = 1,
+    @id_compra = 1,
+    @importe_pagado = 500000;
+--Modificar un detalle pago
+EXEC sp_ManageDetallesPago
+    @accion = 'M', --modificar
+    @id_detalle_pago = 10,
+    @id_compra = 2, 
+    @importe_pagado = 600000;
+--Dar de Baja un detallePago
+EXEC sp_ManageDetallesPago
+    @accion = 'B',--baja
+    @id_detalle_pago = 10;
+
+SELECT * FROM Detalles_Pagos
